e1f9673b12d21f67fce9eaa55485040a457ec27e
Nikita Sidorov <4428551+shlyahten@users.noreply.github.com>
2026-04-28 18:57:32 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlin {
-        jvmToolchain(17)
-    }
     buildFeatures {
         compose = true
     }
