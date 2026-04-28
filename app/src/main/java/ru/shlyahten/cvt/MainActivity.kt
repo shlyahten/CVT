c40246b88bb3ba9115940da1ffe5d6d485fad456
Nikita Sidorov <4428551+shlyahten@users.noreply.github.com>
2026-04-28 19:13:55 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/shlyahten/cvt/MainActivity.kt
+++ app/src/main/java/ru/shlyahten/cvt/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
